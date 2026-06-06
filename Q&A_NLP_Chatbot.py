edde18ac330e00bcc5011dedaedbecd5c39e1077
Mayam99 <141515170+Mayam99@users.noreply.github.com>
2026-06-06 23:56:17 +0530

    Update Q&A_NLP_Chatbot.py

--- Q&A_NLP_Chatbot.py
+++ Q&A_NLP_Chatbot.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 import os
+import faiss
 import torch
 from sentence_transformers import SentenceTransformer
 from transformers import pipeline
